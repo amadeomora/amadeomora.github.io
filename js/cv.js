83485eb304e2be15fdbd89d3bf1bcf9584ccb4c2
Amadeo Mora <amadeo.mora@educa.madrid.org>
2025-08-09 17:09:54 +0200

    json auto

--- js/cv.js
+++ js/cv.js
@@ -1,39 +1,33 @@
-window.addEventListener('load', function() {
-    menu()
-    cv()
+window.addEventListener('load', async function() {
+	const menu = document.querySelector("nav")
+    const main = document.querySelector("main")
+
+    const secciones = await fetchUrl('./data/menu.json')
+    secciones.forEach(it => {
+        menu.innerHTML += `<a href="#${it.id}">${it.link}</a>`
+        main.innerHTML += `<section id="${it.id}"></section>`
+        document.getElementById(it.id).innerHTML = `<h2>${it.titulo}</h2>`
+        seccion(it.id)
+    })
 })
 
-async function menu() {
-    const secciones = await fetchUrl('./data/menu.json')
-	const menuNav = document.querySelector("nav")
-    secciones.forEach(it => {
-		menuNav.innerHTML += `<a href="#${it.id}">${it.link}</a>`
-		document.getElementById(it.id).innerHTML = `<h2>${it.titulo}</h2>`
-	})
+async function seccion(id) {
+    const data = await fetchUrl('./data/cv_'+id+'.json')
+    data.forEach(it => document.getElementById(id).innerHTML += window['tpl_'+id](it))
 }
 
-async function cv() {
-    const cv = await fetchUrl('./data/cv-minify.json')
-    titulos(cv.titulos, 'idTitulos')
-    puestos(cv.puestos, 'idPuestos')
-    ponencias(cv.ponencias, 'idPonencias')
-    libros(cv.libros, 'idLibros')
-    proyectos(cv.proyectos, 'idProyectos')
-    cursos(cv.cursos, 'idCursos')
-}
-
-function titulos(info, section) {
-	info.forEach(it => document.getElementById(section).innerHTML += `
+function tpl_titulos(it) {
+    return `
         <div class="titulacion">
 			<div class="logo"><img src="img/universidad/${it.logo}" alt="Logo de la Universidad"></div>
             <div class="titulo">${it.titulo}</div>
             <div class="universidad">${it.universidad}</div>
         </div>
-    `)
+    `
 }
 
-function puestos(info, section) {
-	info.forEach(it => document.getElementById(section).innerHTML += `
+function tpl_puestos(it) {
+	return `
         <div class="puesto">
             <div class="empresa">${it.empresa}</div>
             <div class="fecha">${it.fecha}</div>
@@ -41,46 +35,46 @@ function puestos(info, section) {
             <div class="modulos">${fmt(it.modulos)}</div>
             <div class="descripcion">${fmt(it.descripcion)}</div>
         </div>
-	`)
+	`
 }
 
-function proyectos(info, section) {
-	info.forEach(it => document.getElementById(section).innerHTML += `
+function tpl_proyectos(it) {
+	return `
         <div class="proyecto">
             <div class="empresa">${it.empresa}</div>
             <div class="desc">${fmt(it.descripcion)}</div>
             <div class="herramientas">${fmt(it.herramientas)}</div>
         </div>
-    `)
+    `
 }
 
-function ponencias(info, section) {
-	info.forEach(it => document.getElementById(section).innerHTML += `
+function tpl_ponencias(it) {
+	return `
         <div class="ponencia">
             <div class="titulo">${it.titulo}</div>
             <div class="lugar">${it.lugar}</div>
         </div>
-    `)
+    `
 }
 
-function libros(info, section) {
-	info.forEach(it => document.getElementById(section).innerHTML += `
+function tpl_libros(it) {
+	return `
         <div class="libro">
             <div class="portada"><img src="img/libros/${it.img}" alt="Portada del libro"></div>
             <div class="titulo">${it.titulo}</div>
             <div class="editorial">${it.editorial}</div>
             <div class="autoria">${it.autoria}</div>
         </div>
-    `)
+    `
 }
 
-function cursos(info, section) {
-	info.forEach(it => document.getElementById(section).innerHTML += `
+function tpl_cursos(it) {
+	return `
         <div class="ponencia">
             <div class="curso">${it.curso}</div>
             <div class="lugar">${it.lugar}</div>
         </div>
-    `)
+    `
 }
 
 // Funciones auxiliares
@@ -92,8 +86,6 @@ fmt = (it) => {
 
 async function fetchUrl(url) {
     const response = await fetch(url)
-    if (!response.ok) {
-        console.log(response)
-    }
+    if (!response.ok) console.log(response)
     return await response.json()
 }
